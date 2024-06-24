ffbc2c384af35ef910480552a11aaf98c4ba8775
Faridullah Qaderi <fareedtech99@gmail.com>
2024-06-23 21:08:59 -0700

    just test for inheriting the existing module fa.po file and its modification

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from odoo.tools.translate import _
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'egp test',
+    'version': '1.0',
+    'summary': 'for test',
+    'description': 'MCIT-Afghanistan EGP test',
+    'category': 'Custom',
+    'sequence': -3,
+    'author': 'Faridullah Qaderi',
+    'depends': ['hr'],
+    'data': [
+        'i18n/fa.po',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
